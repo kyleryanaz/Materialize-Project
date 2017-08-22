0995901dd2b0927c301c91edadd131986d16a0dd
Kyle Ryan <kyleryan480@gmail.com>
2017-08-22 07:44:56 -0700

    Css Working

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 var express = require("express");
 var path = require("path");
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.use(express.static("./views/"));
 
-// homepage
+// serve entire public directory with HTML & CSS files inside
+app.use(express.static(path.resolve(__dirname + "/public")));
+
+// adjust routes with new path to views directory
 app.get("/", function(req, res) {
-  res.sendfile(path.resolve(__dirname + "/views/index.html"));
+  res.sendfile(path.resolve(__dirname + "/public/views/index.html"));
 });
 
 // about page
 app.get("/about", function(req, res) {
-  res.sendfile(path.resolve(__dirname + "/views/about.html"));
+  res.sendfile(path.resolve(__dirname + "/public/views/about.html"));
+});
+
+// store page
+app.get("/store", function(req, res) {
+  res.sendfile(path.resolve(__dirname + "/public/views/store.html"));
+});
+
+// contact page
+app.get("/contact", function(req, res) {
+  res.sendfile(path.resolve(__dirname + "/public/views/contact.html"));
 });
 
 // error page
 app.get("*", function(req, res) {
-  res.sendfile(path.resolve(__dirname + "/views/404.html"));
+  res.sendfile(path.resolve(__dirname + "/public/views/404.html"));
 });
 app.listen(port, function() {
   console.log("Listening on port " + port);
